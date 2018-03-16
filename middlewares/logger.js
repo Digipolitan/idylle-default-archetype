5440c0e78109c324c2dd198d1268f011c7d31ca3
Julien Sarazin <julien.sarazin@me.com>
2018-03-16 14:53:28 +0100

    updated default archetype

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,2 +1,2 @@
 const morgan = require('morgan');
-module.exports = app => app.server.use(morgan(app.settings.logger.level));
+module.exports = app => app.router.use(morgan(app.settings.logger.level));
